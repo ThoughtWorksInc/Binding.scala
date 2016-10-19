bfe196a2a8817bb4908b9fa5e13b2a5138de39cb
Yang Bo <atryyang@thoughtworks.com>
2016-10-19 16:27:04 +0800

    Add more tests

--- route/src/test/scala/com/thoughtworks/binding/RouteSpec.scala
+++ route/src/test/scala/com/thoughtworks/binding/RouteSpec.scala
@@ -9,10 +9,16 @@ import org.scalajs.dom.window
   */
 final class RouteSpec extends FreeSpec with Matchers {
   "route" in {
-
     val myState = Var[Option[Int]](None)
     Route.watchHash(myState)
     window.location.hash should be("#[]")
-
+    myState := Some(1)
+    window.location.hash should be("#[1]")
+    window.location.hash = "#[4]"
+    window.onhashchange(null)
+    myState.get should be(Some(4))
+    window.location.hash = "#[]"
+    window.onhashchange(null)
+    myState.get should be(None)
   }
 }
