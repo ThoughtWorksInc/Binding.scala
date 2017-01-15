19e560b8c97a40d7bdfa313e0a62cef2f772c16d
Yang Bo <atryyang@thoughtworks.com>
2017-01-15 20:35:27 +0800

    Rename CustomTagSpec to NotJavaFXSpec

--- fxml/src/test/scala/com/thoughtworks/binding/NotJavaFXSpec.scala
+++ fxml/src/test/scala/com/thoughtworks/binding/NotJavaFXSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.{FreeSpec, Inside, Matchers, _}
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
-final class CustomTagSpec extends FreeSpec with Matchers with Inside {
+final class NotJavaFXSpec extends FreeSpec with Matchers with Inside {
   "Random" in {
     import java.util._
     @fxml val random = <Random seed={1}/>
