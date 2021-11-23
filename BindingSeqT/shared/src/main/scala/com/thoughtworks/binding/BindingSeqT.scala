e2dcb8354555c59de12cdb3677a813bd719ed454
Yang, Bo <pop.atry@gmail.com>
2021-11-22 18:55:09 -0800

    Simplify the implementation by calculating the active slices with the help of FingerTree

--- BindingSeqT/shared/src/main/scala/com/thoughtworks/binding/BindingSeqT.scala
+++ BindingSeqT/shared/src/main/scala/com/thoughtworks/binding/BindingSeqT.scala
@@ -1,38 +1,34 @@
 package com.thoughtworks.binding
 
-import scalaz.StreamT
-import scala.concurrent.Future
-import scalaz.Monad
-import scalaz.Nondeterminism
-import scalaz.Applicative
-import scalaz.StreamT.Skip
-import scalaz.StreamT.Done
-import scalaz.StreamT.Yield
-import scalaz.StreamT.Step
-import scalaz.IList
 import scalaz.-\/
-import scalaz.\/-
-import scalaz.Monoid
-import scalaz.ICons
-import scalaz.INil
-import scalaz.Isomorphism.IsoFunctor
-import scalaz.Isomorphism.IsoFunctorTemplate
-import scalaz.MonadPlus
-import scalaz.IsomorphismMonadPlus
-import scalaz.Functor
-import scalaz.Free
-import scalaz.Traverse
-import scalaz.FingerTree
-import scalaz.Reducer
-import scalaz.UnitReducer
+import scalaz.Applicative
 import scalaz.DList
+import scalaz.FingerTree
+import scalaz.Free
+import scalaz.IList
+import scalaz.Maybe
+import scalaz.Monad
+import scalaz.Monoid
+import scalaz.Nondeterminism
+import scalaz.Reducer
+import scalaz.StreamT
+import scalaz.StreamT.Done
+import scalaz.StreamT.Skip
+import scalaz.StreamT.Yield
+import scalaz.UnitReducer
+import scalaz.\/-
 
 object BindingSeqT:
-  sealed trait Patch[A]
+  sealed trait Patch[A]:
+    def withOffset(offset: Int): Patch[A]
+    def sizeIncremental: Int
+
   object Patch:
     // TODO: Support move items
     // final case class Move[A](oldIndex: Int, offset: Int, moveCount: Int) extends Patch[A]
-    final case class Splice[A](index: Int, deleteCount: Int, newItems: Iterable[A]) extends Patch[A]
+    final case class Splice[A](index: Int, deleteCount: Int, newItems: Iterable[A]) extends Patch[A]:
+      def withOffset(offset: Int) = copy(index = index + offset)
+      def sizeIncremental = newItems.size - deleteCount
 
   given [M[_]](using M: Nondeterminism[M]): Monad[[a] =>> BindingSeqT[M, a]] with
     def point[A](a: => A): BindingSeqT[M, A] =
@@ -41,228 +37,136 @@ object BindingSeqT:
       val toStepB = { (a: A) =>
         f(a).step
       }
-      type ChooseSubSteps = M[
-        (
-            (
-                SourceIndex,
-                MappedIndex,
-                Step[
-                  Patch[B],
-                  BindingSeqT[M, B]
-                ]
-            ),
-            scalaz.IList[
-              M[
-                (
-                    SourceIndex,
-                    MappedIndex,
-                    Step[
-                      Patch[B],
-                      BindingSeqT[M, B]
-                    ]
-                )
-              ]
-            ]
+      type EventStep[A] = StreamT.Step[Patch[A], BindingSeqT[M, A]]
+      final case class SliceEvent(eventStep: EventStep[B], sliceIndex: Int)
+      final case class Measure(
+          numberOfSlices: Int,
+          numberOfElements: Int,
+          // TODO: Create an opaque type implementation for DList for better performance
+          sliceEventBuilder: (numberOfPreviousSlices: Int) => DList[M[SliceEvent]]
+      )
+      sealed trait Slice:
+        def size: Int
+      final case class ActiveSlice(size: Int, eventQueue: M[EventStep[B]]) extends Slice
+      final case class InactiveSlice(size: Int) extends Slice
+      val emptyEventBuilder = { (numberOfPreviousSlices: Int) => DList.mkDList[M[SliceEvent]](Free.pure) }
+      given Monoid[Measure] with
+        def zero = Measure(0, 0, emptyEventBuilder)
+        def append(f1: Measure, f2: => Measure) =
+          Measure(
+            f1.numberOfSlices + f2.numberOfSlices,
+            f1.numberOfElements + f2.numberOfElements,
+            (f1.sliceEventBuilder, f2.sliceEventBuilder) match
+              case (`emptyEventBuilder`, `emptyEventBuilder`) =>
+                // Optimization the event builder where both subtrees are inactive
+                emptyEventBuilder
+              case (builder1, builder2) =>
+                val numberOfSlices1 = f1.numberOfSlices
+                locally { numberOfPreviousSlices =>
+                  builder1(numberOfPreviousSlices) ++ builder2(numberOfPreviousSlices + numberOfSlices1)
+                }
+          )
+      given Reducer[Slice, Measure] = UnitReducer { (slice: Slice) =>
+        Measure(
+          1,
+          slice.size,
+          slice match {
+            case InactiveSlice(_) =>
+              emptyEventBuilder
+            case ActiveSlice(_, eventQueue) => { (numberOfPreviousSlices: Int) =>
+              DList.mkDList { rest =>
+                Free.pure((M.map(eventQueue)(SliceEvent(_, numberOfPreviousSlices)) :: rest))
+              }
+            }
+          }
         )
-      ]
-      type ChooseSubStepsOption = Option[ChooseSubSteps]
-      type MappedIndex = Int
-      type SourceIndex = Int
-      type MappedSize = Int
-      type SourceSize = Int
-      type SizeMapping = (SourceSize, MappedSize)
-
-      def mapICons[A, B](icons: ICons[A])(f: A => B): ICons[B] = {
-        icons.map(f).asInstanceOf[ICons[B]]
       }
 
-      def bindSubSteps(
-          subSteps: ICons[(SourceIndex, MappedIndex, M[StreamT.Step[Patch[B], BindingSeqT[M, B]]])]
-      )(
-          // TODO: optimize to reuse chooseSubStepsOption when subSteps are not changed
-          chooseSubSteps: ChooseSubSteps = M
-            .chooseAny(subSteps.map { case (sourceIndex, mappedIndex, mStepB) =>
-              M.map(mStepB)((sourceIndex, mappedIndex, _))
-            })
-            .get
-      ): M[Step[Patch[B], BindingSeqT[M, B]]] =
-        M.map(chooseSubSteps) { case ((sourceIndex, mappedIndex, stepB), mSubSteps) =>
-          stepB match
-            case Yield(patch @ Patch.Splice(index, deleteCount, newItems), s) =>
-              Yield(
-                patch.copy(index = index + mappedIndex),
-                () =>
-                  StreamT(
-                    bindSubSteps(
-                      mapICons(subSteps) { subStep =>
-                        if (subStep._1 > sourceIndex) {
-                          subStep.copy(_2 = subStep._2 - deleteCount + newItems.size)
-                        } else if (subStep._1 == sourceIndex) {
-                          subStep.copy(_3 = s().step)
-                        } else {
-                          subStep
-                        }
-                      }
-                    )()
-                  )
-              )
-            case Skip(s) =>
-              Skip(() =>
-                StreamT(
-                  bindSubSteps(
-                    mapICons(subSteps) { subStep =>
-                      if (subStep._1 == sourceIndex) {
-                        subStep.copy(_3 = s().step)
-                      } else {
-                        subStep
-                      }
-                    }
-                  )()
-                )
-              )
-            case Done() =>
-              subSteps.filter(_._1 != sourceIndex) match
-                case restStepBs: ICons[_] =>
-                  Skip(() =>
-                    StreamT(
-                      bindSubSteps(
-                        restStepBs
-                      )()
-                    )
-                  )
-                case INil() =>
-                  Done()
-        }
-
-      def bindStep(
-          mStepA: M[StreamT.Step[Patch[A], BindingSeqT[M, A]]],
-          sizeMappingTree: FingerTree[SizeMapping, SizeMapping],
-          subSteps: IList[(SourceIndex, MappedIndex, M[StreamT.Step[Patch[B], BindingSeqT[M, B]]])]
-      )(
-          // TODO: optimize to reuse chooseSubStepsOption when subSteps are not changed
-          chooseSubStepsOption: ChooseSubStepsOption = M.chooseAny(subSteps.map {
-            case (sourceIndex, mappedIndex, mStepB) =>
-              M.map(mStepB)((sourceIndex, mappedIndex, _))
-          })
-      ): M[Step[Patch[B], BindingSeqT[M, B]]] =
-        def handleStepA(
-            stepA: StreamT.Step[Patch[A], BindingSeqT[M, A]]
-        ): Step[Patch[B], BindingSeqT[M, B]] =
-          stepA match
-            case Yield(Patch.Splice(index, deleteCount, newItems), s) =>
+      def mergeEventQueue(
+          upstreamEventQueueOption: Option[M[EventStep[A]]],
+          sliceTree: FingerTree[Measure, Slice]
+      ): Option[M[EventStep[B]]] =
+        def handleUpstreamEvent(upstreamEvent: EventStep[A]): EventStep[B] =
+          upstreamEvent match
+            case Yield(Patch.Splice(patchIndex, numberOfSlicesDeleted, newItems), s) =>
               // TODO: Avoid unnecessary FingerTree operations to optimize the performance
-              val (left, notLeft) = sizeMappingTree.split { case (sourceIndex, _) =>
-                sourceIndex < index
-              }
-              val (deleted, right) = notLeft.split { case (sourceIndex, _) =>
-                sourceIndex < deleteCount
-              }
+              val (left, notLeft) = sliceTree.split(_.numberOfSlices < patchIndex)
+              val (deleted, right) = notLeft.split(_.numberOfSlices < numberOfSlicesDeleted)
               // TODO: Use measureMonoid once https://github.com/scalaz/scalaz/pull/2234 is merged
-              val (`deleteCount`, numberOfMappedItemsDeleted) = deleted.measure.getOrElse((0, 0))
-
-              val numberOfSourceItemsAdd = newItems.size
-              val newSizeMappingTree = newItems.foldLeft(left) { (tree, _) =>
-                tree :+ (1, 0)
+              val Measure(`patchIndex`, mappedIndex, _) = left.measure.getOrElse(Monoid[Measure].zero)
+              val Measure(`numberOfSlicesDeleted`, numberOfItemsDeleted, _) =
+                deleted.measure.getOrElse(Monoid[Measure].zero)
+              val numberOfSlicesAdd = newItems.size
+              val newSliceTree = newItems.foldLeft(left) { (tree, a) =>
+                tree :+ ActiveSlice(0, toStepB(a))
               } <++> right
-
-              val numberOfSourceItemsIncreased = numberOfSourceItemsAdd - deleteCount
-              val (`index`, mappedIndex) = left.measure.getOrElse((0, 0))
-              val newSubSteps = subSteps.foldRight(
-                IList.empty[(SourceSize, MappedSize, M[StreamT.Step[Patch[B], BindingSeqT[M, B]]])]
-              ) { (subStep, tail) =>
-                val (sourceIndex, mappedIndex, mSubStep) = subStep
-                val adjustedTail = if (sourceIndex >= index + deleteCount) {
-                  (sourceIndex + numberOfSourceItemsAdd, mappedIndex - numberOfMappedItemsDeleted, mSubStep) :: tail
-                } else if (sourceIndex >= index) {
-                  tail
-                } else {
-                  subStep :: tail
-                }
-                if (sourceIndex == index) {
-                  newItems.view.zipWithIndex.foldRight(adjustedTail) { case ((newItem, i), adjustedTail) =>
-                    (index + i, mappedIndex, toStepB(newItem)) :: adjustedTail
-                  }
-                } else {
-                  adjustedTail
-                }
-              }
-              val next = () => StreamT(bindStep(s().step, newSizeMappingTree, newSubSteps)())
-              if (numberOfMappedItemsDeleted > 0) {
-                Yield(Patch.Splice(mappedIndex, numberOfMappedItemsDeleted, Nil), next)
+              val Some(next) = mergeEventQueue(Some(s().step), newSliceTree)
+              if (numberOfItemsDeleted > 0) {
+                Yield(Patch.Splice(mappedIndex, numberOfItemsDeleted, Nil), () => StreamT(next))
               } else {
-                Skip(next)
+                Skip(() => StreamT(next))
               }
             case Skip(s) =>
-              Skip(() => StreamT(bindStep(s().step, sizeMappingTree, subSteps)()))
+              val Some(next) = mergeEventQueue(Some(s().step), sliceTree)
+              Skip(() => StreamT(next))
             case Done() =>
-              subSteps match
-                case nonEmptySubSteps: ICons[_] =>
-                  Skip(() => StreamT(bindSubSteps(nonEmptySubSteps)()))
-                case INil() =>
+              mergeEventQueue(None, sliceTree) match
+                case Some(next) =>
+                  Skip(() => StreamT(next))
+                case None =>
                   Done()
-        chooseSubStepsOption match
+        def handleSliceEvent(sliceEvent: SliceEvent): EventStep[B] =
+          val sliceIndex = sliceEvent.sliceIndex
+          val (left, ActiveSlice(numberOfElementsInActiveSlice, _), right) =
+            sliceTree.split1(_.numberOfSlices < sliceIndex)
+          sliceEvent.eventStep match
+            case Yield(patch, s) =>
+              val newSliceTree =
+                left.add1(ActiveSlice(numberOfElementsInActiveSlice + patch.sizeIncremental, s().step), right)
+              val Some(next) = mergeEventQueue(upstreamEventQueueOption, newSliceTree)
+              val patchWithOffset = left.measure
+                .map { leftMeasure =>
+                  patch.withOffset(leftMeasure.numberOfElements)
+                }
+                .getOrElse(patch)
+              Yield(patchWithOffset, () => StreamT(next))
+            case Skip(s) =>
+              val newSliceTree = left.add1(ActiveSlice(numberOfElementsInActiveSlice, s().step), right)
+              val Some(next) = mergeEventQueue(upstreamEventQueueOption, newSliceTree)
+              Skip(() => StreamT(next))
+            case Done() =>
+              val newSliceTree = left <++> right
+              mergeEventQueue(upstreamEventQueueOption, newSliceTree) match
+                case Some(next) =>
+                  Skip(() => StreamT(next))
+                case None =>
+                  Done()
+        val chooseSliceEventOption = sliceTree.measure.toOption.flatMap { measure =>
+          M.chooseAny(measure.sliceEventBuilder(0).toIList)
+        }
+        upstreamEventQueueOption match
           case None =>
-            M.map(mStepA)(handleStepA(_))
-          case Some(chooseSubSteps) =>
-            M.map(M.choose(mStepA, chooseSubSteps)) {
-              case -\/((stepA, _)) =>
-                handleStepA(stepA)
-              case \/-((mStepA, ((sourceIndex, mappedIndex, stepB), _))) =>
-                stepB match
-                  case Yield(patch @ Patch.Splice(index, deleteCount, newItems), s) =>
-                    val (left, (1, mappedSize), right) = sizeMappingTree.split1 { case (nodeSourceIndex, _) =>
-                      nodeSourceIndex < sourceIndex
-                    }
-                    val newSizeMappingTree = left.add1((1, mappedSize - deleteCount + newItems.size), right)
-                    Yield(
-                      patch.copy(index = index + mappedIndex),
-                      () =>
-                        StreamT(
-                          bindStep(
-                            mStepA,
-                            newSizeMappingTree,
-                            subSteps.map { subStep =>
-                              if (subStep._1 > sourceIndex) {
-                                subStep.copy(_2 = subStep._2 - deleteCount + newItems.size)
-                              } else if (subStep._1 == sourceIndex) {
-                                subStep.copy(_3 = s().step)
-                              } else {
-                                subStep
-                              }
-                            }
-                          )()
-                        )
-                    )
-                  case Skip(s) =>
-                    Skip(() =>
-                      StreamT(
-                        bindStep(
-                          mStepA,
-                          sizeMappingTree,
-                          subSteps.map { subStep =>
-                            if (subStep._1 == sourceIndex) {
-                              subStep.copy(_3 = s().step)
-                            } else {
-                              subStep
-                            }
-                          }
-                        )()
-                      )
-                    )
-                  case Done() =>
-                    Skip(() =>
-                      StreamT(
-                        bindStep(
-                          mStepA,
-                          sizeMappingTree,
-                          subSteps.filter(_._1 != sourceIndex)
-                        )()
-                      )
-                    )
-            }
-      import scalaz.std.tuple.given
-      import scalaz.std.anyVal.given
-      import scalaz.std.option.given
-      StreamT(bindStep(fa.step, FingerTree.empty(Reducer.identityReducer), IList.empty)())
+            chooseSliceEventOption match
+              case None =>
+                None
+              case Some(chooseEvent) =>
+                Some(M.map(chooseEvent) { case (sliceEvent, _) =>
+                  handleSliceEvent(sliceEvent)
+                })
+          case Some(upstreamEventQueue) =>
+            chooseSliceEventOption match
+              case None =>
+                Some(M.map(upstreamEventQueue) { upstreamEvent =>
+                  handleUpstreamEvent(upstreamEvent)
+                })
+              case Some(chooseEvent) =>
+                Some(M.map(M.choose(upstreamEventQueue, chooseEvent)) {
+                  case \/-((_, (sliceEvent, _))) =>
+                    handleSliceEvent(sliceEvent)
+                  case -\/((upstreamEvent, _)) =>
+                    handleUpstreamEvent(upstreamEvent)
+                })
+      val Some(mergedEventQueue) = mergeEventQueue(Some(fa.step), FingerTree.empty)
+      StreamT(mergedEventQueue)
 
 opaque type BindingSeqT[M[_], A] = StreamT[M, BindingSeqT.Patch[A]]
