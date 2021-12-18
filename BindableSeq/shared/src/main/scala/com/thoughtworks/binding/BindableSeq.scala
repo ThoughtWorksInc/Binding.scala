5f476a984422b47412fe87a0cdcf16214681ff3f
Yang, Bo <pop.atry@gmail.com>
2021-12-18 11:54:41 -0800

    Add a BindableSeq instance to lift plain value to a BindingSeq

--- BindableSeq/shared/src/main/scala/com/thoughtworks/binding/BindableSeq.scala
+++ BindableSeq/shared/src/main/scala/com/thoughtworks/binding/BindableSeq.scala
@@ -1,4 +1,6 @@
 package com.thoughtworks.binding
+
+import scalaz.Applicative
 object BindableSeq:
   type Aux[-From, Value0] = BindableSeq[From] {
     type Value = Value0
@@ -6,6 +8,10 @@ object BindableSeq:
   type Lt[-From, +Value0] = BindableSeq[From] {
     type Value <: Value0
   }
+  given [A](using Applicative[Binding.Awaitable]): BindableSeq.Aux[A, A] =
+    new BindableSeq[A]:
+      type Value = A
+      def toBindingSeq(a: A): Binding.BindingSeq[A] = Binding.Constants(a)
 
 trait BindableSeq[-From]:
   type Value
