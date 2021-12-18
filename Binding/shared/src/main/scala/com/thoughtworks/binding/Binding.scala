0d68082fdc35de2c10678a15cccb24166c8ebd54
Yang, Bo <pop.atry@gmail.com>
2021-12-18 01:04:13 -0800

    Reformat Binding.scala

--- Binding/shared/src/main/scala/com/thoughtworks/binding/Binding.scala
+++ Binding/shared/src/main/scala/com/thoughtworks/binding/Binding.scala
@@ -32,14 +32,20 @@ object Binding:
     def apply[M[_]: Applicative, A](a: A): BindingT[M, A] = a :: StreamT.empty
     given [M[_]](using M: Nondeterminism[M]): Monad[[X] =>> BindingT[M, X]] with
       def point[A](a: => A) = BindingT(a)
-      def bind[A, B](upstream: BindingT[M, A])(f: A => BindingT[M, B]): BindingT[M, B] =
+      def bind[A, B](upstream: BindingT[M, A])(
+          f: A => BindingT[M, B]
+      ): BindingT[M, B] =
         given [B]: Equal[B] = Equal.equalA[B]
         // We could avoid the `asInstanceOf` by wrapping `f(_)` like this:
         //   upstream.flatMapLatest(f(_)).distinctUntilChanged
         // but it will create an unnecessary function object.
         // Use `asInstanceOf` anyway
-        upstream.flatMapLatest(f.asInstanceOf[A => StreamT[M, B]]).distinctUntilChanged
-      override def map[A, B](upstream: BindingT[M, A])(f: A => B): BindingT[M, B] =
+        upstream
+          .flatMapLatest(f.asInstanceOf[A => StreamT[M, B]])
+          .distinctUntilChanged
+      override def map[A, B](upstream: BindingT[M, A])(
+          f: A => B
+      ): BindingT[M, B] =
         given Equal[B] = Equal.equalA[B]
         upstream.map(f).distinctUntilChanged
 
@@ -55,25 +61,38 @@ object Binding:
     object Patch:
       // TODO: Support move items
       // final case class Move[A](oldIndex: Int, offset: Int, moveCount: Int) extends Patch[A]
-      final case class Splice[+A](index: Int, deleteCount: Int, newItems: Iterable[A]) extends Patch[A]:
-        private[BindingSeqT] def withOffset(offset: Int) = copy(index = index + offset)
+      final case class Splice[+A](
+          index: Int,
+          deleteCount: Int,
+          newItems: Iterable[A]
+      ) extends Patch[A]:
+        private[BindingSeqT] def withOffset(offset: Int) =
+          copy(index = index + offset)
         private[BindingSeqT] def sizeIncremental = newItems.size - deleteCount
 
     extension [M[_], A](upstream: BindingSeqT[M, A])
-      /** Returns a new data-binding sequence by applying a function to all elements of this sequence and using the
-        * elements of the resulting collections.
+      /** Returns a new data-binding sequence by applying a function to all
+        * elements of this sequence and using the elements of the resulting
+        * collections.
         *
-        * Whenever `upstream` or one of the subsequence changes, the result sequence changes accordingly. The time
-        * complexity to update the result sequence, when `upstream` changes, is `O(log(n) + c)`, where `n` is the size
-        * of `upstream`, and `c` is number of elements in `upstream`, mapped to mutable subsequences by `f`, of which
-        * the indices in `upstream` are changing. For example, if the size of `upstream` is 10 and 4 elements of them
-        * mapped to mutable subsequences, when an element is prepended to `upstream`, `c` is 4. However when an element
-        * is appended to `upstream`, `c` is zero, because no element's index in `upstream is changed.
+        * Whenever `upstream` or one of the subsequence changes, the result
+        * sequence changes accordingly. The time complexity to update the result
+        * sequence, when `upstream` changes, is `O(log(n) + c)`, where `n` is
+        * the size of `upstream`, and `c` is number of elements in `upstream`,
+        * mapped to mutable subsequences by `f`, of which the indices in
+        * `upstream` are changing. For example, if the size of `upstream` is 10
+        * and 4 elements of them mapped to mutable subsequences, when an element
+        * is prepended to `upstream`, `c` is 4. However when an element is
+        * appended to `upstream`, `c` is zero, because no element's index in
+        * `upstream is changed.
         *
-        * The time complexity to update the result sequence, when one of the subsequence changes, is `O(log(n))`, where
-        * `n` is the size of `upstream`.
+        * The time complexity to update the result sequence, when one of the
+        * subsequence changes, is `O(log(n))`, where `n` is the size of
+        * `upstream`.
         */
-      def flatMap[B](f: A => BindingSeqT[M, B])(using M: Nondeterminism[M]): BindingSeqT[M, B] =
+      def flatMap[B](f: A => BindingSeqT[M, B])(using
+          M: Nondeterminism[M]
+      ): BindingSeqT[M, B] =
         val toStepB = { (a: A) =>
           f(a).step
         }
@@ -84,13 +103,18 @@ object Binding:
             numberOfSlices: Int,
             numberOfElements: Int,
             // TODO: Create an opaque type implementation for DList for better performance
-            sliceEventBuilder: Option[(numberOfPreviousSlices: Int) => M[SliceEvent]]
+            sliceEventBuilder: Option[
+              (numberOfPreviousSlices: Int) => M[SliceEvent]
+            ]
         )
         sealed trait Slice:
           def size: Int
-        final case class ActiveSlice(size: Int, eventQueue: M[EventStep[B]]) extends Slice
+        final case class ActiveSlice(size: Int, eventQueue: M[EventStep[B]])
+            extends Slice
         final case class InactiveSlice(size: Int) extends Slice
-        val emptyEventBuilder = { (numberOfPreviousSlices: Int) => DList.mkDList[M[SliceEvent]](Free.pure) }
+        val emptyEventBuilder = { (numberOfPreviousSlices: Int) =>
+          DList.mkDList[M[SliceEvent]](Free.pure)
+        }
         given Monoid[Measure] with
           def zero = Measure(0, 0, None)
           def append(f1: Measure, f2: => Measure) =
@@ -102,19 +126,24 @@ object Binding:
                 case (None, None) =>
                   None
                 case (None, Some(builder2)) =>
-                  Some(Memo.immutableHashMapMemo { (numberOfPreviousSlices: Int) =>
-                    builder2(numberOfPreviousSlices + numberOfSlices1)
+                  Some(Memo.immutableHashMapMemo {
+                    (numberOfPreviousSlices: Int) =>
+                      builder2(numberOfPreviousSlices + numberOfSlices1)
                   })
                 case (someBuilder1: Some[_], None) =>
                   someBuilder1
                 case (Some(builder1), Some(builder2)) =>
-                  Some(Memo.immutableHashMapMemo { (numberOfPreviousSlices: Int) =>
-                    M.map(
-                      M.choose(builder1(numberOfPreviousSlices), builder2(numberOfPreviousSlices + numberOfSlices1))
-                    ) {
-                      case -\/((sliceEvent1, _)) => sliceEvent1
-                      case \/-((_, sliceEvent2)) => sliceEvent2
-                    }
+                  Some(Memo.immutableHashMapMemo {
+                    (numberOfPreviousSlices: Int) =>
+                      M.map(
+                        M.choose(
+                          builder1(numberOfPreviousSlices),
+                          builder2(numberOfPreviousSlices + numberOfSlices1)
+                        )
+                      ) {
+                        case -\/((sliceEvent1, _)) => sliceEvent1
+                        case \/-((_, sliceEvent2)) => sliceEvent2
+                      }
                   })
             )
         given Reducer[Slice, Measure] = UnitReducer { (slice: Slice) =>
@@ -125,8 +154,9 @@ object Binding:
               case InactiveSlice(_) =>
                 None
               case ActiveSlice(_, eventQueue) =>
-                Some(Memo.immutableHashMapMemo { (numberOfPreviousSlices: Int) =>
-                  M.map(eventQueue)(SliceEvent(_, numberOfPreviousSlices))
+                Some(Memo.immutableHashMapMemo {
+                  (numberOfPreviousSlices: Int) =>
+                    M.map(eventQueue)(SliceEvent(_, numberOfPreviousSlices))
                 })
             }
           )
@@ -149,17 +179,23 @@ object Binding:
                     s
                   ) =>
                 // TODO: Avoid unnecessary FingerTree operations to optimize the performance
-                val (left, notLeft) = sliceTree.split(_.numberOfSlices < patchIndex)
-                val (deleted, right) = notLeft.split(_.numberOfSlices < numberOfSlicesDeleted)
+                val (left, notLeft) =
+                  sliceTree.split(_.numberOfSlices < patchIndex)
+                val (deleted, right) =
+                  notLeft.split(_.numberOfSlices < numberOfSlicesDeleted)
                 val Measure(`patchIndex`, mappedIndex, _) = left.measureMonoid
-                val Measure(`numberOfSlicesDeleted`, numberOfItemsDeleted, _) = deleted.measureMonoid
+                val Measure(`numberOfSlicesDeleted`, numberOfItemsDeleted, _) =
+                  deleted.measureMonoid
                 val numberOfSlicesAdd = newItems.size
                 val newSliceTree = newItems.foldLeft(left) { (tree, a) =>
                   tree :+ ActiveSlice(0, toStepB(a))
                 } <++> right
                 val Some(next) = mergeEventQueue(Some(s().step), newSliceTree)
                 if (numberOfItemsDeleted > 0) {
-                  Yield(Patch.Splice(mappedIndex, numberOfItemsDeleted, Nil), () => StreamT(next))
+                  Yield(
+                    Patch.Splice(mappedIndex, numberOfItemsDeleted, Nil),
+                    () => StreamT(next)
+                  )
                 } else {
                   Skip(() => StreamT(next))
                 }
@@ -179,8 +215,15 @@ object Binding:
             sliceEvent.eventStep match
               case Yield(patch, s) =>
                 val newSliceTree =
-                  left.add1(ActiveSlice(numberOfElementsInActiveSlice + patch.sizeIncremental, s().step), right)
-                val Some(next) = mergeEventQueue(upstreamEventQueueOption, newSliceTree)
+                  left.add1(
+                    ActiveSlice(
+                      numberOfElementsInActiveSlice + patch.sizeIncremental,
+                      s().step
+                    ),
+                    right
+                  )
+                val Some(next) =
+                  mergeEventQueue(upstreamEventQueueOption, newSliceTree)
                 val patchWithOffset = left.measure
                   .map { leftMeasure =>
                     patch.withOffset(leftMeasure.numberOfElements)
@@ -188,8 +231,12 @@ object Binding:
                   .getOrElse(patch)
                 Yield(patchWithOffset, () => StreamT(next))
               case Skip(s) =>
-                val newSliceTree = left.add1(ActiveSlice(numberOfElementsInActiveSlice, s().step), right)
-                val Some(next) = mergeEventQueue(upstreamEventQueueOption, newSliceTree)
+                val newSliceTree = left.add1(
+                  ActiveSlice(numberOfElementsInActiveSlice, s().step),
+                  right
+                )
+                val Some(next) =
+                  mergeEventQueue(upstreamEventQueueOption, newSliceTree)
                 Skip(() => StreamT(next))
               case Done() =>
                 val newSliceTree = left <++> right
@@ -198,8 +245,9 @@ object Binding:
                     Skip(() => StreamT(next))
                   case None =>
                     Done()
-          val chooseSliceEventOption = sliceTree.measure.toOption.flatMap { measure =>
-            measure.sliceEventBuilder.map(_(0))
+          val chooseSliceEventOption = sliceTree.measure.toOption.flatMap {
+            measure =>
+              measure.sliceEventBuilder.map(_(0))
           }
           upstreamEventQueueOption match
             case None =>
@@ -221,18 +269,22 @@ object Binding:
                     case -\/((upstreamEvent, _)) =>
                       handleUpstreamEvent(upstreamEvent)
                   })
-        val Some(mergedEventQueue) = mergeEventQueue(Some(upstream.step), FingerTree.empty)
+        val Some(mergedEventQueue) =
+          mergeEventQueue(Some(upstream.step), FingerTree.empty)
         StreamT(mergedEventQueue)
 
-    given [M[_]](using M: Nondeterminism[M]): Monad[[X] =>> BindingSeqT[M, X]] with
+    given [M[_]](using M: Nondeterminism[M]): Monad[[X] =>> BindingSeqT[M, X]]
+      with
       def point[A](a: => A): BindingSeqT[M, A] =
         BindingSeqT(a)
 
-      def bind[A, B](upstream: BindingSeqT[M, A])(f: A => BindingSeqT[M, B]): BindingSeqT[M, B] =
+      def bind[A, B](upstream: BindingSeqT[M, A])(
+          f: A => BindingSeqT[M, B]
+      ): BindingSeqT[M, B] =
         upstream.flatMap(f)
 
-  /** The data binding expression of a sequence, essentially an asynchronous stream of patches describing how the
-    * sequence is changing.
+  /** The data binding expression of a sequence, essentially an asynchronous
+    * stream of patches describing how the sequence is changing.
     */
   type BindingSeq[+A] = BindingSeqT[Future, A]
   object BindingSeq:
