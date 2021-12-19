492512c2f49f75a2d7a8653550e03f8a2f0aa429
Yang, Bo <pop.atry@gmail.com>
2021-12-18 18:20:35 -0800

    Put the document on the method instead of the extension group

--- BindingT/shared/src/main/scala/com/thoughtworks/binding/package.scala
+++ BindingT/shared/src/main/scala/com/thoughtworks/binding/package.scala
@@ -29,14 +29,15 @@ extension [V,A](tree: FingerTree[V, A])
   private def measureMonoid(implicit V: Monoid[V]): V =
     tree.fold(V.zero, (v, _) => v, (v, _, _, _) => v)
 
-// Copied from https://github.com/scalaz/scalaz/pull/2231
-/** The [[flatMapLatest]] operator behaves much like the [[mergeMap]] except that whenever a new item is emitted by the
-  * source [[StreamT]], it will not subscribe to and stop mirroring the [[StreamT]] that was generated from the
-  * previously-emitted item, and begin only mirroring the current one.
-  *
-  * <img style="max-width: 100%" src="http://reactivex.io/documentation/operators/images/flatMapLatest.png"/>
-  */
 extension [M[_], A](fa: StreamT[M, A])
+
+  // Copied from https://github.com/scalaz/scalaz/pull/2231
+  /** The [[flatMapLatest]] operator behaves much like the [[mergeMap]] except that whenever a new item is emitted by the
+    * source [[StreamT]], it will not subscribe to and stop mirroring the [[StreamT]] that was generated from the
+    * previously-emitted item, and begin only mirroring the current one.
+    *
+    * <img style="max-width: 100%" src="http://reactivex.io/documentation/operators/images/flatMapLatest.png"/>
+    */
   private def flatMapLatest[B](f: A => StreamT[M, B])(implicit M: Nondeterminism[M]): StreamT[M, B] = {
     def flatMapLatestInitStep(fsa: M[Step[A, StreamT[M, A]]]): M[Step[B, StreamT[M, B]]] = {
       M.map(fsa) {
