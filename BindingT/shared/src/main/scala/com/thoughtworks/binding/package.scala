6bc0439d0bfd50d000cf343689363d8a92c8d955
Yang, Bo <pop.atry@gmail.com>
2021-12-18 22:39:19 -0800

    Add StreamT.scan and StreamT.scanLeft

--- BindingT/shared/src/main/scala/com/thoughtworks/binding/package.scala
+++ BindingT/shared/src/main/scala/com/thoughtworks/binding/package.scala
@@ -30,6 +30,50 @@ extension [V,A](tree: FingerTree[V, A])
     tree.fold(V.zero, (v, _) => v, (v, _, _, _) => v)
 
 extension [M[_], A](fa: StreamT[M, A])
+  private def stepMap[B](f: Step[A, StreamT[M, A]] => Step[B, StreamT[M, B]])(implicit M: Functor[M]): StreamT[M, B] = StreamT(M.map(fa.step)(f))
+ 
+  // Copied from https://github.com/scalaz/scalaz/pull/2250
+  private def scanLeftTail[B](head: B)(op: (B, A) => B)(implicit M: Functor[M]): StreamT[M, B] =
+    stepMap[B] {
+      case Yield(a, s) =>
+        val next = op(head, a)
+        Yield(next, () => s().scanLeftTail(next)(op))
+      case Skip(s) =>
+        Skip(() => s().scanLeftTail(head)(op))
+      case Done() =>
+        Done()
+    }
+
+  /** Produces a [[StreamT]] containing cumulative results of applying the
+    * operator going left to right, including the initial value.
+    *
+    *
+    *  @tparam B      the type of the elements in the resulting collection
+    *  @param head    the initial value
+    *  @param op      the binary operator applied to the intermediate result and the element
+    *  @return        collection with intermediate results
+    */
+  private[binding] def scanLeft[B](head: B)(op: (B, A) => B)(implicit M: Applicative[M]): StreamT[M, B] =
+    head :: scanLeftTail(head)(op)
+
+  /** Computes a prefix scan of the elements of the collection.
+    *
+    *  Note: The neutral element `B` may be applied more than once.
+    *
+    *  @tparam B         element type of the resulting collection
+    *  @param op         the associative operator for the scan
+    *
+    *  @return           a new [[StreamT]] containing the prefix scan of the elements in this $coll
+    */
+  private[binding] def scan[B >: A](op: (B, B) => B)(implicit M: Functor[M]): StreamT[M, B] =
+    stepMap[B] {
+      case Yield(a, s) =>
+        Yield(a, () => s().scanLeftTail[B](a)(op))
+      case Skip(s) =>
+        Skip(() => s().scan(op))
+      case Done() =>
+        Done()
+    }
 
   // Copied from https://github.com/scalaz/scalaz/pull/2231
   /** The [[flatMapLatest]] operator behaves much like the [[mergeMap]] except that whenever a new item is emitted by the
@@ -38,7 +82,7 @@ extension [M[_], A](fa: StreamT[M, A])
     *
     * <img style="max-width: 100%" src="http://reactivex.io/documentation/operators/images/flatMapLatest.png"/>
     */
-  private def flatMapLatest[B](f: A => StreamT[M, B])(implicit M: Nondeterminism[M]): StreamT[M, B] = {
+  private[binding] def flatMapLatest[B](f: A => StreamT[M, B])(implicit M: Nondeterminism[M]): StreamT[M, B] = {
     def flatMapLatestInitStep(fsa: M[Step[A, StreamT[M, A]]]): M[Step[B, StreamT[M, B]]] = {
       M.map(fsa) {
         case Yield(a, s) =>
