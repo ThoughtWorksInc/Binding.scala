2d1a27f2a7296632b08abe7dd3fba52e6d2d2494
Yang, Bo <pop.atry@gmail.com>
2021-12-18 17:04:31 -0800

    Switch to AsyncFreeSpec

--- html/src/test/scala/com/thoughtwors/binding/htmlSpec.scala
+++ html/src/test/scala/com/thoughtwors/binding/htmlSpec.scala
@@ -1,17 +1,16 @@
 package com.thoughtworks.binding
-import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.should.Matchers
-import scala.concurrent.ExecutionContext.Implicits.given
 import scalaz.std.scalaFuture.given
 import com.thoughtworks.dsl.Dsl
 import Binding.BindingSeq
 import org.scalajs.dom.Node
 
 // TODO: Move to html.scala once sbt-example supports extensions
-final class htmlSpec extends AnyFreeSpec with Matchers {
+final class htmlSpec extends AsyncFreeSpec with Matchers {
 
   "html literal of multiple root nodes should be a BindingSeq[Node]" in {
-    def myId = "xx"
+    def myId = "my-id"
     val nodes = html"<p>1&nbsp;${"foo"}2${"bar"}3</p><div id=$myId class=\"my-class\">${"baz"}</div>"
     nodes should be(a[BindingSeq[Node]])
   }
