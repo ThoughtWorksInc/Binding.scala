32e6cb33751aa9391afec8dbf0430a947a7fd41c
Yang, Bo <pop.atry@gmail.com>
2022-01-01 23:43:37 -0800

    Add a comment about Coyoneda

--- DefaultFuture/shared/src/main/scala/com/thoughtworks/binding/DefaultFuture.scala
+++ DefaultFuture/shared/src/main/scala/com/thoughtworks/binding/DefaultFuture.scala
@@ -3,7 +3,11 @@ package com.thoughtworks.binding
 import scala.concurrent.*
 import scalaz.*
 
-/** A [[scala.concurrent.Future]] whose scalaz instances are not orphan */
+/** A [[scala.concurrent.Future]] whose scalaz instances are not orphan
+  * @todo
+  *   Replace `Future` with `Coyoneda[F, _]` once
+  *   https://github.com/scalaz/scalaz/pull/2269 is merged
+  */
 opaque type DefaultFuture[+A] >: Future[A] <: Future[A] = Future[A]
 object DefaultFuture:
   export scalaz.std.scalaFuture.*
