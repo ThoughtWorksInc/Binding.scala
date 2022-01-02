b7127d178b491902a114762fad5811f5a1d1b60d
Yang, Bo <pop.atry@gmail.com>
2022-01-01 23:43:37 -0800

    Add a Dsl[Bind[...]] instance for Kleisli domain
    Because the generic monad transformer requires a type signature incompatible with Kleisli in terms of partial unification

--- keywords-Bind/shared/src/main/scala/com/thoughtworks/binding/keywords/Bind.scala
+++ keywords-Bind/shared/src/main/scala/com/thoughtworks/binding/keywords/Bind.scala
@@ -3,13 +3,7 @@ package keywords
 import com.thoughtworks.dsl.Dsl
 import com.thoughtworks.binding.CovariantStreamT
 import com.thoughtworks.binding.StreamT.*
-import scalaz.Hoist
-import scalaz.~>
-import scalaz.Functor
-import scalaz.Monad
-import scalaz.Equal
-import scalaz.Nondeterminism
-import scalaz.MonadTrans
+import scalaz.{Bind => _, *}
 
 opaque type Bind[M[_], A] <: Dsl.Keyword.Opaque =
   Dsl.Keyword.Opaque.Of[CovariantStreamT[M, A]]
@@ -42,6 +36,26 @@ object Bind {
         .flatMapLatest(handler)
         .distinctUntilChanged
   }
+
+  /** @note
+    *   We need this instance because the generic instance for monad transformer
+    *   does not work for ReaderT / Kleisli, as Kleisli's type parameter order
+    *   is incompatible with partial unification
+    */
+  given [E, M[_], A, B](using
+      Nondeterminism[M],
+      Nondeterminism[Kleisli[M, E, *]]
+  ): Dsl.Original[Bind[M, A], CovariantStreamT[Kleisli[M, E, *], B], A] =
+    Dsl.Original { (keyword, handler) =>
+      @inline given [B]: Equal[B] = Equal.equalA[B]
+      Bind.apply
+        .flip(keyword)
+        .trans(new (M ~> Kleisli[M, E, *]) {
+          def apply[A](ma: M[A]) = Kleisli(_ => ma)
+        })
+        .flatMapLatest(handler)
+        .distinctUntilChanged
+    }
 
   extension [FA, M[_], A](inline fa: FA)(using
       inline notKeyword: util.NotGiven[
