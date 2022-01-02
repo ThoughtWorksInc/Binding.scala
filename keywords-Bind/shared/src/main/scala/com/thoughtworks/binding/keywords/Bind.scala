3ac5b0cbf4204309eb057fa61a0fd3c32b52745f
Yang, Bo <pop.atry@gmail.com>
2022-01-01 23:43:37 -0800

    Add Dsl instance for Bind keyword in a monad transformer of CovariantStreamT

--- keywords-Bind/shared/src/main/scala/com/thoughtworks/binding/keywords/Bind.scala
+++ keywords-Bind/shared/src/main/scala/com/thoughtworks/binding/keywords/Bind.scala
@@ -3,8 +3,14 @@ package keywords
 import com.thoughtworks.dsl.Dsl
 import com.thoughtworks.binding.CovariantStreamT
 import com.thoughtworks.binding.StreamT.*
+import scalaz.Hoist
+import scalaz.~>
+import scalaz.Functor
+import scalaz.Monad
 import scalaz.Equal
 import scalaz.Nondeterminism
+import scalaz.MonadTrans
+
 opaque type Bind[M[_], A] <: Dsl.Keyword.Opaque =
   Dsl.Keyword.Opaque.Of[CovariantStreamT[M, A]]
 
@@ -17,8 +23,24 @@ object Bind {
       Nondeterminism[M]
   ): Dsl.Original[Bind[M, A], CovariantStreamT[M, B], A] = Dsl.Original {
     (keyword, handler) =>
-      given [B]: Equal[B] = Equal.equalA[B]
+      @inline given [B]: Equal[B] = Equal.equalA[B]
       keyword.flatMapLatest(handler).distinctUntilChanged
+  }
+
+  given [F[_[_], _], M[_], A, B](using
+      Nondeterminism[F[M, _]],
+      MonadTrans[F],
+      Monad[M]
+  ): Dsl.Original[Bind[M, A], CovariantStreamT[F[M, *], B], A] = Dsl.Original {
+    (keyword, handler) =>
+      @inline given [B]: Equal[B] = Equal.equalA[B]
+      Bind.apply
+        .flip(keyword)
+        .trans(new (M ~> F[M, *]) {
+          def apply[A](ma: M[A]) = MonadTrans[F].liftM(ma)
+        })
+        .flatMapLatest(handler)
+        .distinctUntilChanged
   }
 
   extension [FA, M[_], A](inline fa: FA)(using
